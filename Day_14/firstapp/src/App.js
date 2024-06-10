dd77987d4ed7ac838a772124f70df5dc0a9313de
Amit180745 <thakuramit9019@gmail.com>
2024-06-11 00:25:37 +0530

    Child Component

--- Day_14/firstapp/src/App.js
+++ Day_14/firstapp/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import HelloWorld from './components/HelloWorld';
+
 function App() {
   return (
     <div>
       <h1>Hello World!</h1>
-    <div>
-      This is my first React Project.
+    <h2>This is my first React Project.</h2>
       <div>
       <button>Click Me!</button>
       </div>
-    </div>
+      <HelloWorld/>
     </div>
   );
 }
