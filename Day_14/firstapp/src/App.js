e2160d6a3384b93e05b343951d5ef43231a1d348
Amit180745 <thakuramit9019@gmail.com>
2024-06-13 20:22:07 +0530

    props in ReactJs

--- Day_14/firstapp/src/App.js
+++ Day_14/firstapp/src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
-    <h2>This is my first React Project.</h2>
-      <div>
-      <button>Click Me!</button>
-      </div>
-      <HelloWorld/>
     </div>
   );
 }
--- Day_14/firstapp/src/index.js
+++ Day_14/firstapp/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import TaskList from './components/TaskList';
-import Counter from './components/Counter/Counter';
 import TodoApp from './components/TodoApp/TodoApp';
 
 
@@ -11,9 +8,6 @@ import TodoApp from './components/TodoApp/TodoApp';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <React.StrictMode>
-     {/* <App /> */}
-     {/* <TaskList/> */}
-     {/* <Counter/> */}
      <TodoApp/>
    </React.StrictMode>
 );
